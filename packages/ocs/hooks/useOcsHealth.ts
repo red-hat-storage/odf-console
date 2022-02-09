e971af39ced2c85b93b1212b7cf60ee5b4c792ad
Bipul Adhikari <badhikar@redhat.com>
2022-02-09 14:14:50 +0545

    (fix) Show OCS subcomponent health issue in UI

---
+++ packages/ocs/hooks/useOcsHealth.ts
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import { K8sResourceKind } from '@odf/shared/types';
+import { referenceForModel } from '@odf/shared/utils';
+import {
+  HealthState,
+  useK8sWatchResource,
+  WatchK8sResource,
+} from '@openshift-console/dynamic-plugin-sdk';
+import { usePrometheusPoll } from '@openshift-console/dynamic-plugin-sdk-internal';
+import { useTranslation } from 'react-i18next';
+import {
+  CephClusterModel,
+  CephObjectStoreModel,
+  NooBaaSystemModel,
+} from '../models/core';
+import { Health, HEALTH_QUERY } from '../queries';
+import {
+  getCephHealthState,
+  getNooBaaState,
+  getRGWHealthState,
+} from '../utils';
+
+const cephClusterResource: WatchK8sResource = {
+  kind: referenceForModel(CephClusterModel),
+  isList: true,
+};
+
+const cephObjectStoreResource: WatchK8sResource = {
+  kind: referenceForModel(CephObjectStoreModel),
+  isList: true,
+};
+
+const noobaaResource: WatchK8sResource = {
+  kind: referenceForModel(NooBaaSystemModel),
+  isList: true,
+};
+
+// Not applicable
+const NA = 'N/A';
+
+const AcceptableHealthStates = [
+  HealthState.OK,
+  HealthState.LOADING,
+  HealthState.UPDATING,
+  HealthState.NOT_AVAILABLE,
+  NA,
+];
+
+export const useGetOCSHealth = () => {
+  const { t } = useTranslation('plugin__odf-console');
+  const [cephData, cephLoaded, cephLoadError] =
+    useK8sWatchResource<K8sResourceKind[]>(cephClusterResource);
+  const [cephObjData, cephObjLoaded, cephObjLoadError] = useK8sWatchResource<
+    K8sResourceKind[]
+  >(cephObjectStoreResource);
+  const [noobaaData, noobaaLoaded, noobaaLoadError] =
+    useK8sWatchResource<K8sResourceKind[]>(noobaaResource);
+
+  const [noobaaHealthStatus, noobaaQueryLoadError] = usePrometheusPoll({
+    query: HEALTH_QUERY[Health.NOOBAA],
+    endpoint: 'api/v1/query' as any,
+  });
+
+  const cephHealthState = getCephHealthState(
+    { ceph: { data: cephData, loaded: cephLoaded, loadError: cephLoadError } },
+    t
+  ).state;
+  const interimRGWState =
+    !cephObjLoadError && cephObjLoaded
+      ? getRGWHealthState(cephObjData[0]).state
+      : NA;
+
+  const interimMCGState = getNooBaaState(
+    [
+      {
+        response: noobaaHealthStatus,
+        error: noobaaQueryLoadError,
+      },
+    ],
+    t,
+    {
+      loaded: noobaaLoaded,
+      loadError: noobaaLoadError,
+      data: noobaaData,
+    }
+  ).state;
+
+  const mcgState = AcceptableHealthStates.includes(interimMCGState)
+    ? HealthState.OK
+    : HealthState.ERROR;
+
+  const rgwState = AcceptableHealthStates.includes(interimRGWState)
+    ? HealthState.OK
+    : HealthState.ERROR;
+
+  const cephStorageHealthStatus = AcceptableHealthStates.includes(
+    cephHealthState
+  )
+    ? HealthState.OK
+    : HealthState.ERROR;
+
+  const unifiedObjectHealth =
+    mcgState === HealthState.ERROR || rgwState === HealthState.ERROR
+      ? HealthState.ERROR
+      : HealthState.OK;
+
+  const unifiedHealthState = React.useMemo(() => {
+    if (
+      unifiedObjectHealth === HealthState.ERROR &&
+      cephStorageHealthStatus === HealthState.ERROR
+    ) {
+      return {
+        rawHealthState: '2',
+        errorMessages: [
+          t('Block and File service is unhealthy'),
+          t('Object service is unhealthy'),
+        ],
+      };
+    } else if (unifiedObjectHealth === HealthState.ERROR) {
+      return {
+        rawHealthState: '1',
+        errorMessages: [t('Object service is unhealthy')],
+        errorComponent: rgwState !== HealthState.OK ? 'block-file' : 'object',
+      };
+    } else if (cephStorageHealthStatus === HealthState.ERROR) {
+      return {
+        rawHealthState: '1',
+        errorMessages: [t('Block and File service is unhealthy')],
+        errorComponent: 'block-file',
+      };
+    }
+    return {
+      rawHealthState: '0',
+    };
+  }, [unifiedObjectHealth, cephStorageHealthStatus, rgwState, t]);
+
+  return unifiedHealthState;
+};
--- packages/odf/components/odf-dashboard/status-card/storage-system-popup.tsx
+++ packages/odf/components/odf-dashboard/status-card/storage-system-popup.tsx
@@ -3,15 +3,19 @@ import Status, { StatusPopupSection } from '@odf/shared/popup/status-popup';
 import { HealthState } from '@openshift-console/dynamic-plugin-sdk';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { Flex, FlexItem } from '@patternfly/react-core';
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
+  ExclamationTriangleIcon,
 } from '@patternfly/react-icons';
+import './storage-system-popup.scss';
 
 type SystemHealthMap = {
   systemName: string;
   healthState: HealthState;
   link: string;
+  extraTexts?: string[];
 };
 
 type StorageSystemPopopProps = {
@@ -21,6 +25,9 @@ type StorageSystemPopopProps = {
 const healthStateToIcon = {
   [HealthState.OK]: (
     <CheckCircleIcon color="var(--pf-global--primary-color--100)" />
+  ),
+  [HealthState.WARNING]: (
+    <ExclamationTriangleIcon color="var(--pf-global--warning-color--100)" />
   ),
   [HealthState.ERROR]: (
     <ExclamationCircleIcon color="var(--pf-global--danger-color--100)" />
@@ -41,7 +48,20 @@ const StorageSystemPopup: React.FC<StorageSystemPopopProps> = ({
           key={system.systemName}
           icon={healthStateToIcon[system.healthState]}
         >
-          <Link to={system.link}>{system.systemName}</Link>
+          <Flex direction={{ default: 'column' }}>
+            <FlexItem className="odf-storageSystemPopup__item--margin">
+              <Link to={system.link}>{system.systemName}</Link>
+            </FlexItem>
+            {!!system.extraTexts && (
+              <FlexItem>
+                {system.extraTexts.map((extraText, i) => (
+                  <div className="text-muted" key={i}>
+                    {extraText}
+                  </div>
+                ))}
+              </FlexItem>
+            )}
+          </Flex>
         </Status>
       ))}
     </StatusPopupSection>
