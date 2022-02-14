6a4dda2854dc1aa32142e3a6f2fbf8be4f4b25b6
Timothy Asir Jeyasingh <tjeyasin@redhat.com>
2022-02-14 17:34:25 +0530

    Add block-pool test
    Signed-off-by: Timothy Asir Jeyasingh <tjeyasin@redhat.com>

---
+++ cypress/tests/block-pool-create.spec.ts
@@ -0,0 +1,19 @@
+import { createBlockPool, deleteBlockPoolFromCli } from '../views/block-pool';
+
+/** @depricated testcases are moved to odf-console */
+xdescribe('Test block pool creation under ODF UI', () => {
+  before(() => {
+    cy.login();
+    cy.visit('/');
+    cy.install();
+  });
+
+  after(() => {
+    cy.logout();
+  });
+
+  it('Check for a new pool creation', () => {
+    createBlockPool();
+    deleteBlockPoolFromCli();
+  });
+});
