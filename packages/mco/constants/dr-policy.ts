6641dd7de402cc32cd39b164c0984f6891013a8e
Afreen Rahman <afrahman@redhat.com>
2022-03-15 22:11:00 +0530

    Add create DR policy page
    - adds all UI elements
    - no integration with backend

---
+++ packages/mco/constants/dr-policy.ts
@@ -0,0 +1,8 @@
+import { TFunction } from 'i18next';
+
+export const MAX_ALLOWED_CLUSTERS = 2;
+
+export const REPLICATION_TYPE = (t: TFunction) => ({
+  async: t('plugin__odf-console~Asynchronous'),
+  sync: t('plugin__odf-console~Synchronous'),
+});
