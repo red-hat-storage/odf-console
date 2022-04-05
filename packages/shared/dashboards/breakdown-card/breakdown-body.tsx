62f9d1fa78e5a53ec008f5728462080f7a83283d
Bipul Adhikari <badhikar@redhat.com>
2022-04-05 10:05:32 +0545

    Migrate common dashboard packages

---
+++ packages/shared/dashboards/breakdown-card/breakdown-body.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { Humanize } from '@openshift-console/dynamic-plugin-sdk';
+import { K8sKind } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+import { useTranslation } from 'react-i18next';
+import { Grid, GridItem } from '@patternfly/react-core';
+import { TotalCapacityBody } from './breakdown-capacity';
+import { BreakdownChart, LabelPadding } from './breakdown-chart';
+import { BreakdownChartLoading } from './breakdown-loading';
+import { addAvailable, StackDataPoint, getLegends } from './utils';
+
+export const BreakdownCardBody: React.FC<BreakdownBodyProps> = ({
+  top5MetricsStats,
+  metricTotal,
+  capacityUsed,
+  capacityAvailable,
+  metricModel,
+  humanize,
+  isLoading,
+  hasLoadError,
+  ocsVersion = '',
+  labelPadding,
+}) => {
+  const { t } = useTranslation();
+
+  if (isLoading && !hasLoadError) {
+    return <BreakdownChartLoading />;
+  }
+  if (!capacityUsed || !top5MetricsStats.length || hasLoadError) {
+    return (
+      <div className="text-secondary capacity-breakdown-card--error">
+        {t('Not available')}
+      </div>
+    );
+  }
+  if (capacityUsed === '0') {
+    return (
+      <div className="text-secondary capacity-breakdown-card--error">
+        {t('Not enough usage data')}
+      </div>
+    );
+  }
+
+  const chartData = addAvailable(top5MetricsStats, capacityAvailable, metricTotal, humanize, t);
+
+  const legends = getLegends(chartData);
+
+  // Removes Legend for available
+  if (capacityAvailable) {
+    legends.pop();
+  }
+
+  return (
+    <Grid>
+      <GridItem span={4}>
+        <TotalCapacityBody
+          capacity={humanize(capacityUsed).string}
+          suffix={t('used')}
+        />
+      </GridItem>
+      <GridItem span={4} />
+      <GridItem span={4}>
+        {capacityAvailable && (
+          <TotalCapacityBody
+            capacity={humanize(capacityAvailable).string}
+            suffix={t('available')}
+            className="capacity-breakdown-card__available-body text-secondary"
+          />
+        )}
+      </GridItem>
+      <GridItem span={12}>
+        <BreakdownChart
+          data={chartData}
+          legends={legends}
+          metricModel={metricModel}
+          ocsVersion={ocsVersion}
+          labelPadding={labelPadding}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export type BreakdownBodyProps = {
+  isLoading: boolean;
+  hasLoadError: boolean;
+  metricTotal: string;
+  top5MetricsStats: StackDataPoint[];
+  capacityUsed: string;
+  capacityAvailable?: string;
+  metricModel: K8sKind;
+  humanize: Humanize;
+  ocsVersion?: string;
+  labelPadding?: LabelPadding;
+};
--- packages/shared/utils/metrics.ts
+++ packages/shared/utils/metrics.ts
@@ -2,3 +2,14 @@ import { PrometheusResponse } from "@openshift-console/dynamic-plugin-sdk";
 
 export const getGaugeValue = (response: PrometheusResponse) =>
   response?.data?.result?.[0]?.value?.[1];
+
+export const getResiliencyProgress = (results: PrometheusResponse): number => {
+  /**
+   * Possible values for progress:
+   *   - A float value of String type
+   *   - 'NaN'
+   *   - undefined
+   */
+  const progress: string = getGaugeValue(results);
+  return parseFloat(progress);
+};
