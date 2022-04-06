84ff3c1e349299941cc6869a3d21912c891b868d
Bipul Adhikari <badhikar@redhat.com>
2022-04-06 15:07:30 +0545

    Migrate Storage Efficiency Card(Persistent Internal)

---
+++ packages/ocs/dashboards/persistent-internal/storage-efficiency-card/storage-efficiency-card.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { EfficiencyItemBody } from '@odf/shared/dashboards/storage-efficiency/storage-efficiency-card-item';
+import { getGaugeValue, humanizeBinaryBytes } from '@odf/shared/utils';
+import { usePrometheusPoll } from '@openshift-console/dynamic-plugin-sdk-internal';
+import { useTranslation } from 'react-i18next';
+import { Card, CardBody, CardHeader, CardTitle } from '@patternfly/react-core';
+import {
+  POOL_STORAGE_EFFICIENCY_QUERIES,
+  StorageDashboardQuery,
+} from '../../../queries';
+
+const StorageEfficiencyCard: React.FC = () => {
+  const { t } = useTranslation();
+
+  const [poolCapacityRatioResult, poolCapacityRatioResultError] =
+    usePrometheusPoll({
+      query:
+        POOL_STORAGE_EFFICIENCY_QUERIES[
+          StorageDashboardQuery.POOL_CAPACITY_RATIO
+        ],
+      endpoint: null,
+    });
+
+  const [poolSavedResult, poolSavedResultError] = usePrometheusPoll({
+    query:
+      POOL_STORAGE_EFFICIENCY_QUERIES[
+        StorageDashboardQuery.POOL_CAPACITY_RATIO
+      ],
+    endpoint: null,
+  });
+  const ratio = getGaugeValue(poolCapacityRatioResult);
+  const saved = getGaugeValue(poolSavedResult);
+
+  const compressionStats = () => {
+    const capacityRatio = Number(ratio);
+    return t('ceph-storage-plugin~{{capacityRatio, number}}:1', {
+      capacityRatio: Math.round(capacityRatio),
+    });
+  };
+
+  const savingStats = () => {
+    const savingsValue = Number(saved);
+    const savedBytes = humanizeBinaryBytes(savingsValue).string;
+    return savedBytes;
+  };
+
+  const compressionRatioProps = {
+    stats: Number(ratio),
+    isLoading: !poolCapacityRatioResult && !poolCapacityRatioResultError,
+    error: !!poolCapacityRatioResultError || !ratio,
+    title: t('ceph-storage-plugin~Compression ratio'),
+    infoText: t(
+      'ceph-storage-plugin~The Compression Ratio represents the compressible data effectiveness metric inclusive of all compression-enabled pools.'
+    ),
+    getStats: compressionStats,
+  };
+
+  const savingsProps = {
+    stats: Number(saved),
+    isLoading: !poolSavedResult && !poolSavedResultError,
+    error: !!poolSavedResultError || !saved,
+    title: t('ceph-storage-plugin~Savings'),
+    infoText: t(
+      'ceph-storage-plugin~The Savings metric represents the actual disk capacity saved inclusive of all compression-enabled pools and associated replicas.'
+    ),
+    getStats: savingStats,
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{t('ceph-storage-plugin~Storage Efficiency')}</CardTitle>
+      </CardHeader>
+      <CardBody>
+        <EfficiencyItemBody {...compressionRatioProps} />
+        <EfficiencyItemBody {...savingsProps} />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default StorageEfficiencyCard;
