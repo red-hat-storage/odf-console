be1c894eb851fc1640f14faf9382fd90cca66c36
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2022-04-20 22:19:44 +0530

    DRPolicy empty list page
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

---
+++ packages/mco/components/empty-state-page/empty-page.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { useCustomTranslation } from '@odf/shared/useCustomTranslationHook';
+import {
+  Button,
+  Title,
+  EmptyState,
+  EmptyStateVariant,
+  EmptyStateIcon,
+  EmptyStateBody,
+} from '@patternfly/react-core';
+import './empty-page.scss';
+
+const EmptyPageIcon: React.FC = () => {
+  const { t } = useCustomTranslation();
+  const image = require('../../assets/EmptyPageIcon.png');
+  return (
+    <img
+      src={image}
+      className="mco-empty-page__icon"
+      alt={t('Empty page logo')}
+    />
+  );
+};
+
+const EmptyPage: React.FC<EmptyPageProps> = (props) => {
+  return (
+    <div className="mco-empty-page__background">
+      <EmptyState variant={EmptyStateVariant.large}>
+        <EmptyStateIcon icon={EmptyPageIcon} />
+        <Title headingLevel="h4" size="lg">
+          {props.title}
+        </Title>
+        <EmptyStateBody>{props.children}</EmptyStateBody>
+        <Button variant="primary" onClick={props.onClick}>
+          {props.buttonText}
+        </Button>
+      </EmptyState>
+    </div>
+  );
+};
+
+type EmptyPageProps = {
+  title: string;
+  children?: any;
+  buttonText: string;
+  onClick: () => void;
+};
+
+export default EmptyPage;
--- packages/mco/constants/dr-policy.ts
+++ packages/mco/constants/dr-policy.ts
@@ -4,7 +4,6 @@ export const ODF_MINIMUM_SUPPORT = '4.11.0';
 export const STORAGE_SYSTEM_NAME = 'ocs-storagecluster-storagesystem';
 export const CEPH_CLUSTER_NAME = 'ocs-storagecluster-cephcluster';
 export const MANAGED_CLUSTER_REGION_CLAIM = 'region.open-cluster-management.io';
-
 export const MAX_ALLOWED_CLUSTERS = 2;
 export const DR_SECHEDULER_NAME = 'ramen';
 
