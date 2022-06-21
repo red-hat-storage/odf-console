f4132938bafe213e170a56b9ba901a825c7d45e2
sanjalkatiyar <sanjaldhir@gmail.com>
2022-06-21 18:11:58 +0530

    Redirect to OCS dashboard without clicking Overview

--- packages/shared/utils/dashboard.ts
+++ packages/shared/utils/dashboard.ts
@@ -9,7 +9,7 @@ import { TFunction } from 'i18next';
 const DASH_PREFIX = '/odf/system';
 
 export const getDashboardLink = (systemKind: string, systemName: string) =>
-  `${DASH_PREFIX}/${systemKind}/${systemName}`;
+  `${DASH_PREFIX}/${systemKind}/${systemName}/overview`;
 
 export const getWorstStatus = (
   componentsHealth: SubsystemHealth[],
