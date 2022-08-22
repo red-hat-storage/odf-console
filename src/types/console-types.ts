940d962c4887be6416c0976f7e53b6bcac2fe651
sanjalkatiyar <sanjaldhir@gmail.com>
2022-08-22 12:41:29 +0530

    Add admin flag

--- src/types/console-types.ts
+++ src/types/console-types.ts
@@ -4,6 +4,43 @@
 
 import { K8sResourceCommon } from "@openshift-console/dynamic-plugin-sdk";
 
+export enum Operator {
+    Exists = 'Exists',
+    DoesNotExist = 'DoesNotExist',
+    In = 'In',
+    NotIn = 'NotIn',
+    Equals = 'Equals',
+    NotEqual = 'NotEqual',
+    GreaterThan = 'GreaterThan',
+    LessThan = 'LessThan',
+    NotEquals = 'NotEquals',
+}
+
+export type MatchExpression = {
+    key: string;
+    operator: Operator | string;
+    values?: string[];
+    value?: string;
+};
+
+export type K8sResourceKind = K8sResourceCommon & {
+    spec?: {
+      selector?: Selector | MatchLabels;
+      [key: string]: any;
+    };
+    status?: { [key: string]: any };
+    data?: { [key: string]: any };
+};
+
+export type Selector = {
+    matchLabels?: MatchLabels;
+    matchExpressions?: MatchExpression[];
+};
+
+export type MatchLabels = {
+    [key: string]: string;
+};
+
 export type ClusterServiceVersionIcon = { base64data: string; mediatype: string };
 
 export enum ClusterServiceVersionStatus {
