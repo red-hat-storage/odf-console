b31f899c1e6873bf8b9b93070e93dd391727a0c6
sanjalkatiyar <sanjaldhir@gmail.com>
2022-09-28 17:21:35 +0530

    store enterprise namespace to configmap

--- packages/odf/components/kms-config/utils.tsx
+++ packages/odf/components/kms-config/utils.tsx
@@ -325,6 +325,7 @@ const createClusterVaultResources = (
     case VaultAuthMethods.KUBERNETES:
       vaultConfigData = {
         ...vaultConfigData,
+        VAULT_NAMESPACE: kms.providerNamespace,
         VAULT_AUTH_KUBERNETES_ROLE: kms.authValue.value,
         VAULT_AUTH_MOUNT_PATH: kms.providerAuthPath,
       };
