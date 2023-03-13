4cf254bcc78b931df88c1a800ef6024742dbaee4
Bipul Adhikari <badhikar@redhat.com>
2023-03-13 17:18:22 +0545

    Fixes rerendering issue Refactors usage of PF components
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- packages/odf/components/topology/sidebar/storage-cluster/StorageClusterObserve.tsx
+++ packages/odf/components/topology/sidebar/storage-cluster/StorageClusterObserve.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { UtilizationContent as ExternalUtilizationContent } from '@odf/ocs/dashboards/persistent-external/utilization-card';
 import { UtilizationContent as InternalUtilizationContent } from '@odf/ocs/dashboards/persistent-internal/utilization-card/utilization-card';
-import { useFlag } from '@openshift-console/dynamic-plugin-sdk';
+import {
+  K8sResourceCommon,
+  useFlag,
+} from '@openshift-console/dynamic-plugin-sdk';
 import { CEPH_FLAG, OCS_INDEPENDENT_FLAG } from '../../../../features';
 
-export const StorageClusterObserve: React.FC = () => {
-  const isIndependent = useFlag(OCS_INDEPENDENT_FLAG);
-  const isCephAvailable = useFlag(CEPH_FLAG);
-  const isInternal = !isIndependent && isCephAvailable;
+export const StorageClusterObserve: React.FC<{ resource?: K8sResourceCommon }> =
+  () => {
+    const isIndependent = useFlag(OCS_INDEPENDENT_FLAG);
+    const isCephAvailable = useFlag(CEPH_FLAG);
+    const isInternal = !isIndependent && isCephAvailable;
 
-  return isInternal ? (
-    <InternalUtilizationContent />
-  ) : (
-    <ExternalUtilizationContent />
-  );
-};
+    return isInternal ? (
+      <InternalUtilizationContent />
+    ) : (
+      <ExternalUtilizationContent />
+    );
+  };
