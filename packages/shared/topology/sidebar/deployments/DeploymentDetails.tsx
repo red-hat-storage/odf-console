13c9e81c311f55ec5875b2cb5d94bfa9ab9a5e47
dpandit <dpandit@li-6f946d4c-287f-11b2-a85c-b0cb07ed936d.ibm.com>
2023-05-29 16:47:48 +0530

    fix: added the section heading for deployment details in the topology view

--- packages/shared/topology/sidebar/deployments/DeploymentDetails.tsx
+++ packages/shared/topology/sidebar/deployments/DeploymentDetails.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { ResourceSummary } from '@odf/shared/details-page/DetailsPage';
+import { SectionHeading } from '@odf/shared/heading/page-heading';
 import { DeploymentModel } from '@odf/shared/models';
 import { DeploymentKind } from '@odf/shared/types';
+import { useCustomTranslation } from '@odf/shared/useCustomTranslationHook';
 
 type DeploymentDetailsProps = {
   resource: DeploymentKind;
@@ -10,8 +12,10 @@ type DeploymentDetailsProps = {
 export const DeploymentDetails: React.FC<DeploymentDetailsProps> = ({
   resource,
 }) => {
+  const { t } = useCustomTranslation();
   return (
     <div className="odf-m-pane__body">
+      <SectionHeading text={t('Deployment details')} />
       <ResourceSummary resource={resource} resourceModel={DeploymentModel} />
     </div>
   );
