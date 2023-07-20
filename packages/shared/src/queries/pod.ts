a2d8385e46be8f920190a6c6bd48e0bfbc29b0dc
Bipul Adhikari <badhikar@redhat.com>
2023-07-20 13:59:52 +0545

    Fixes Pod memory query for Topology sidebar
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- packages/shared/src/queries/pod.ts
+++ packages/shared/src/queries/pod.ts
@@ -7,5 +7,5 @@ export const POD_QUERIES = {
   [PodMetrics.CPU]:
     'pod:container_cpu_usage:sum{namespace="openshift-storage"}',
   [PodMetrics.MEMORY]:
-    'sum(container_memory_working_set_bytes{namespace="openshift-storage"}) BY (pod, namespace)',
+    'sum(container_memory_working_set_bytes{namespace="openshift-storage", container=""}) BY (pod, namespace)',
 };
