89b9ec9ccb463c4d08b59768acc6252e7dd27e7d
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2023-08-22 18:45:54 +0530

    Fetching alerts from OCP thanos instead of ACM thanos
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- packages/mco/components/mco-dashboard/data-policy/alert-card/alert-card.tsx
+++ packages/mco/components/mco-dashboard/data-policy/alert-card/alert-card.tsx
@@ -3,12 +3,11 @@ import { filterDRAlerts } from '@odf/mco/utils';
 import AlertsPanel from '@odf/shared/alert/AlertsPanel';
 import useAlerts from '@odf/shared/monitoring/useAlert';
 import { Card, CardBody } from '@patternfly/react-core';
-import { ACM_ENDPOINT, HUB_CLUSTER_NAME } from '../../../../constants';
 import AlertItem from './alert-item';
 import './alert-card.scss';
 
 export const AlertsCard: React.FC = () => {
-  const [alerts, loaded, loadError] = useAlerts(ACM_ENDPOINT, HUB_CLUSTER_NAME);
+  const [alerts, loaded, loadError] = useAlerts();
 
   return (
     <Card data-test="alerts-card">
