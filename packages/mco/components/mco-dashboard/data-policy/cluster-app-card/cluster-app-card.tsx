GowthamShanmugam/ocp4-mco-ci

Application view for subscription application monitoring
Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

packages/mco/components/mco-dashboard/data-policy/cluster-app-card/cluster-app-card.tsx
@@ -3,6 +3,7 @@ import {
   HUB_CLUSTER_NAME,
   ALL_APPS,
   applicationDetails,
+  APPLICATION_TYPE,
 } from '@odf/mco/constants';
 import {
   DRClusterAppsMap,
@@ -13,6 +14,7 @@ import {
   MirrorPeerKind,
 } from '@odf/mco/types';
 import { getClusterNamesFromMirrorPeers } from '@odf/mco/utils';
+import { useCustomTranslation } from '@odf/shared';
 import { DataUnavailableError } from '@odf/shared/generic/Error';
 import {
   useCustomPrometheusPoll,
@@ -31,6 +33,7 @@ import {
   CardTitle,
   Grid,
   GridItem,
+  Text,
 } from '@patternfly/react-core';
 import {
   ACMManagedClusterViewModel,
@@ -45,7 +48,12 @@ import {
   CSVStatusesContext,
   DRResourcesContext,
 } from '../dr-dashboard-context';
-import { ActivitySection, SnapshotSection } from './argo-application-set';
+import {
+  ActivitySection,
+  SnapshotSection,
+  SubscriptionDetailsSection,
+  SubscriptionSection,
+} from './argo-application-set';
 import {
   HealthSection,
   PeerConnectionSection,
@@ -59,7 +67,63 @@ import {
 } from './common';
 import './cluster-app-card.scss';
 
-export const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
+const ApplicationSetAppCard: React.FC<AppWiseCardProps> = ({
+  protectedPVCData,
+  selectedApp,
+}) => {
+  return (
+    <Grid hasGutter>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <ProtectedPVCsSection
+          protectedPVCData={protectedPVCData}
+          selectedApp={selectedApp}
+        />
+      </GridItem>
+      <GridItem lg={3} rowSpan={8} sm={12}>
+        <ActivitySection selectedApp={selectedApp} />
+      </GridItem>
+      <GridItem lg={9} rowSpan={8} sm={12}>
+        <SnapshotSection selectedApp={selectedApp} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <VolumeSummarySection
+          protectedPVCData={protectedPVCData}
+          selectedApp={selectedApp}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+const SubscriptionSetAppCard: React.FC<AppWiseCardProps> = ({
+  protectedPVCData,
+  selectedApp,
+}) => {
+  return (
+    <Grid hasGutter>
+      <GridItem lg={3} rowSpan={8} sm={12}>
+        <ProtectedPVCsSection
+          protectedPVCData={protectedPVCData}
+          selectedApp={selectedApp}
+        />
+      </GridItem>
+      <GridItem lg={9} rowSpan={8} sm={12}>
+        <SubscriptionSection selectedApp={selectedApp} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <SubscriptionDetailsSection selectedApp={selectedApp} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <VolumeSummarySection
+          protectedPVCData={protectedPVCData}
+          selectedApp={selectedApp}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
   clusterName,
   lastSyncTimeData,
   protectedPVCData,
@@ -111,31 +175,49 @@ export const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
   );
 };
 
-export const AppWiseCard: React.FC<AppWiseCardProps> = ({
-  protectedPVCData,
-  selectedApp,
+const AppWiseCard: React.FC<AppWiseCardProps> = (props) => {
+  const { appType } = props?.selectedApp;
+  switch (appType) {
+    case APPLICATION_TYPE.APPSET:
+      return <ApplicationSetAppCard {...props} />;
+    case APPLICATION_TYPE.SUBSCRIPTION:
+      return <SubscriptionSetAppCard {...props} />;
+    default:
+      return <></>;
+  }
+};
+
+const ClusterAppCardTitle: React.FC<ClusterAppCardTitleProps> = ({
+  app,
+  appKind,
+  appAPIVersion,
+  appType,
+  cluster,
 }) => {
-  return (
-    <Grid hasGutter>
-      <GridItem lg={12} rowSpan={8} sm={12}>
-        <ProtectedPVCsSection
-          protectedPVCData={protectedPVCData}
-          selectedApp={selectedApp}
-        />
-      </GridItem>
-      <GridItem lg={3} rowSpan={8} sm={12}>
-        <ActivitySection selectedApp={selectedApp} />
-      </GridItem>
-      <GridItem lg={9} rowSpan={8} sm={12}>
-        <SnapshotSection selectedApp={selectedApp} />
-      </GridItem>
-      <GridItem lg={12} rowSpan={8} sm={12}>
-        <VolumeSummarySection
-          protectedPVCData={protectedPVCData}
-          selectedApp={selectedApp}
-        />
-      </GridItem>
-    </Grid>
+  const { t } = useCustomTranslation();
+  const apiVersion = `${appKind?.toLowerCase()}.${
+    appAPIVersion?.split('/')[0]
+  }`;
+  const applicationDetailsPath =
+    applicationDetails
+      .replace(':namespace', app.namespace)
+      .replace(':name', app.name) +
+    '?apiVersion=' +
+    apiVersion;
+  return !!app.namespace ? (
+    <div>
+      <Text className="mco-cluster-app__headerText--size mco-dashboard__statusText--margin">
+        {t('Application: ')}
+        <Link id="app-search-argo-apps-link" to={applicationDetailsPath}>
+          {app.name}
+        </Link>
+      </Text>
+      <Text className="mco-dashboard__statusText--margin">
+        {t('Type: {{type}}', { type: appType })}
+      </Text>
+    </div>
+  ) : (
+    <>{cluster}</>
   );
 };
 
@@ -189,15 +271,6 @@ export const ClusterAppCard: React.FC = () => {
       : drClusterAppsMap[cluster]?.protectedApps;
     return filterPVCDataUsingApps(pvcsData, protectedApps);
   }, [drClusterAppsMap, selectedApp, cluster, mcvs, mcvsLoaded, mcvsLoadError]);
-  const apiVersion = `${selectedApp?.appKind?.toLowerCase()}.${
-    selectedApp?.appAPIVersion?.split('/')[0]
-  }`;
-  const applicationDetailsPath =
-    applicationDetails
-      .replace(':namespace', app.namespace)
-      .replace(':name', app.name) +
-    '?apiVersion=' +
-    apiVersion;
 
   return (
     <Card data-test="cluster-app-card">
@@ -212,17 +285,14 @@ export const ClusterAppCard: React.FC = () => {
                 setCluster={setCluster}
                 setApp={setApp}
               />
-              <CardTitle className="mco-cluster-app__text--margin-top mco-cluster-app__headerText--size">
-                {!!app.namespace ? (
-                  <Link
-                    id="app-search-argo-apps-link"
-                    to={applicationDetailsPath}
-                  >
-                    {app.name}
-                  </Link>
-                ) : (
-                  cluster
-                )}
+              <CardTitle className="mco-cluster-app__text--margin-top">
+                <ClusterAppCardTitle
+                  app={app}
+                  cluster={cluster}
+                  appKind={selectedApp?.appKind}
+                  appType={selectedApp?.appType}
+                  appAPIVersion={selectedApp?.appAPIVersion}
+                />
               </CardTitle>
             </div>
           </CardHeader>
@@ -267,4 +337,12 @@ type ClusterWiseCardProps = {
 type AppWiseCardProps = {
   protectedPVCData: ProtectedPVCData[];
   selectedApp: ProtectedAppMap;
+};
+
+type ClusterAppCardTitleProps = {
+  app: AppObj;
+  cluster: string;
+  appKind: string;
+  appAPIVersion: string;
+  appType: APPLICATION_TYPE;
 };

packages/mco/components/modals/app-manage-policies/policy-list-view.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { getPageRange } from '@odf/mco/utils';
 import { ActionDropdown } from '@odf/shared/dropdown/action-dropdown';
 import { ModalBody } from '@odf/shared/modals/Modal';
 import { getName } from '@odf/shared/selectors';
@@ -38,12 +39,6 @@ import './style.scss';
 
 const INITIAL_PAGE_NUMBER = 1;
 const COUNT_PER_PAGE_NUMBER = 4;
-
-const getRange = (currentPage: number, perPage: number) => {
-  const indexOfLastRow = currentPage * perPage;
-  const indexOfFirstRow = indexOfLastRow - perPage;
-  return [indexOfFirstRow, indexOfLastRow];
-};
 
 const filterPolicies = (dataPolicyInfo: DataPolicyType[], searchText: string) =>
   dataPolicyInfo.filter((policy) =>
@@ -130,7 +125,7 @@ export const PolicyListView: React.FC<PolicyListViewProps> = ({
   const [page, setPage] = React.useState(INITIAL_PAGE_NUMBER);
   const [perPage, setPerPage] = React.useState(COUNT_PER_PAGE_NUMBER);
   const [searchText, onSearchChange] = React.useState('');
-  const [start, end] = getRange(page, perPage);
+  const [start, end] = getPageRange(page, perPage);
   const policies = filterPolicies(dataPolicyInfo, searchText) || [];
   const paginatedPolicies = policies.slice(start, end);
   const unFilteredAssignedPolicyLength = dataPolicyInfo.length;

red-hat-storage/odf-console

packages/mco/components/mco-dashboard/data-policy/cluster-app-card/cluster-app-card.tsx
@@ -3,6 +3,7 @@ import {
   HUB_CLUSTER_NAME,
   ALL_APPS,
   applicationDetails,
+  APPLICATION_TYPE,
 } from '@odf/mco/constants';
 import {
   DRClusterAppsMap,
@@ -13,6 +14,7 @@ import {
   MirrorPeerKind,
 } from '@odf/mco/types';
 import { getClusterNamesFromMirrorPeers } from '@odf/mco/utils';
+import { useCustomTranslation } from '@odf/shared';
 import { DataUnavailableError } from '@odf/shared/generic/Error';
 import {
   useCustomPrometheusPoll,
@@ -31,6 +33,7 @@ import {
   CardTitle,
   Grid,
   GridItem,
+  Text,
 } from '@patternfly/react-core';
 import {
   ACMManagedClusterViewModel,
@@ -45,7 +48,12 @@ import {
   CSVStatusesContext,
   DRResourcesContext,
 } from '../dr-dashboard-context';
-import { ActivitySection, SnapshotSection } from './application';
+import {
+  ActivitySection,
+  SnapshotSection,
+  SubscriptionDetailsTable,
+  SubscriptionSection,
+} from './application';
 import {
   HealthSection,
   PeerConnectionSection,
@@ -59,7 +67,63 @@ import {
 } from './common';
 import './cluster-app-card.scss';
 
-export const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
+const ApplicationSetAppCard: React.FC<AppWiseCardProps> = ({
+  protectedPVCData,
+  selectedApplication,
+}) => {
+  return (
+    <Grid hasGutter>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <ProtectedPVCsSection
+          protectedPVCData={protectedPVCData}
+          selectedApplication={selectedApplication}
+        />
+      </GridItem>
+      <GridItem lg={3} rowSpan={8} sm={12}>
+        <ActivitySection selectedApplication={selectedApplication} />
+      </GridItem>
+      <GridItem lg={9} rowSpan={8} sm={12}>
+        <SnapshotSection selectedApplication={selectedApplication} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <VolumeSummarySection
+          protectedPVCData={protectedPVCData}
+          selectedApplication={selectedApplication}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+const SubscriptionSetAppCard: React.FC<AppWiseCardProps> = ({
+  protectedPVCData,
+  selectedApplication,
+}) => {
+  return (
+    <Grid hasGutter>
+      <GridItem lg={3} rowSpan={8} sm={12}>
+        <ProtectedPVCsSection
+          protectedPVCData={protectedPVCData}
+          selectedApplication={selectedApplication}
+        />
+      </GridItem>
+      <GridItem lg={9} rowSpan={8} sm={12}>
+        <SubscriptionSection selectedApplication={selectedApplication} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <SubscriptionDetailsTable selectedApplication={selectedApplication} />
+      </GridItem>
+      <GridItem lg={12} rowSpan={8} sm={12}>
+        <VolumeSummarySection
+          protectedPVCData={protectedPVCData}
+          selectedApplication={selectedApplication}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
   clusterName,
   lastSyncTimeData,
   protectedPVCData,
@@ -111,31 +175,48 @@ export const ClusterWiseCard: React.FC<ClusterWiseCardProps> = ({
   );
 };
 
-export const AppWiseCard: React.FC<AppWiseCardProps> = ({
-  protectedPVCData,
-  selectedApplication,
+const AppWiseCard: React.FC<AppWiseCardProps> = (props) => {
+  switch (props?.selectedApplication?.appType) {
+    case APPLICATION_TYPE.APPSET:
+      return <ApplicationSetAppCard {...props} />;
+    case APPLICATION_TYPE.SUBSCRIPTION:
+      return <SubscriptionSetAppCard {...props} />;
+    default:
+      return <></>;
+  }
+};
+
+const ClusterAppCardTitle: React.FC<ClusterAppCardTitleProps> = ({
+  app,
+  appKind,
+  appAPIVersion,
+  appType,
+  cluster,
 }) => {
-  return (
-    <Grid hasGutter>
-      <GridItem lg={12} rowSpan={8} sm={12}>
-        <ProtectedPVCsSection
-          protectedPVCData={protectedPVCData}
-          selectedApplication={selectedApplication}
-        />
-      </GridItem>
-      <GridItem lg={3} rowSpan={8} sm={12}>
-        <ActivitySection selectedApplication={selectedApplication} />
-      </GridItem>
-      <GridItem lg={9} rowSpan={8} sm={12}>
-        <SnapshotSection selectedApplication={selectedApplication} />
-      </GridItem>
-      <GridItem lg={12} rowSpan={8} sm={12}>
-        <VolumeSummarySection
-          protectedPVCData={protectedPVCData}
-          selectedApplication={selectedApplication}
-        />
-      </GridItem>
-    </Grid>
+  const { t } = useCustomTranslation();
+  const apiVersion = `${appKind?.toLowerCase()}.${
+    appAPIVersion?.split('/')[0]
+  }`;
+  const applicationDetailsPath =
+    applicationDetails
+      .replace(':namespace', app.namespace)
+      .replace(':name', app.name) +
+    '?apiVersion=' +
+    apiVersion;
+  return !!app.namespace ? (
+    <div>
+      <Text className="mco-cluster-app__headerText--size mco-dashboard__statusText--margin">
+        {t('Application: ')}
+        <Link id="app-search-argo-apps-link" to={applicationDetailsPath}>
+          {app.name}
+        </Link>
+      </Text>
+      <Text className="mco-dashboard__statusText--margin">
+        {t('Type: {{type}}', { type: appType })}
+      </Text>
+    </div>
+  ) : (
+    <div className="mco-cluster-app__headerText--size">{cluster}</div>
   );
 };
 
@@ -196,15 +277,6 @@ export const ClusterAppCard: React.FC = () => {
     mcvsLoaded,
     mcvsLoadError,
   ]);
-  const apiVersion = `${selectedApplication?.appKind?.toLowerCase()}.${
-    selectedApplication?.appAPIVersion?.split('/')[0]
-  }`;
-  const applicationDetailsPath =
-    applicationDetails
-      .replace(':namespace', application.namespace)
-      .replace(':name', application.name) +
-    '?apiVersion=' +
-    apiVersion;
 
   return (
     <Card data-test="cluster-app-card">
@@ -219,14 +291,14 @@ export const ClusterAppCard: React.FC = () => {
                 setCluster={setCluster}
                 setApplication={setApplication}
               />
-              <CardTitle className="mco-cluster-app__text--margin-top mco-cluster-app__headerText--size">
-                {!!application.namespace ? (
-                  <Link id="app-overview-link" to={applicationDetailsPath}>
-                    {application.name}
-                  </Link>
-                ) : (
-                  cluster
-                )}
+              <CardTitle className="mco-cluster-app__text--margin-top">
+                <ClusterAppCardTitle
+                  app={application}
+                  cluster={cluster}
+                  appKind={selectedApplication?.appKind}
+                  appType={selectedApplication?.appType}
+                  appAPIVersion={selectedApplication?.appAPIVersion}
+                />
               </CardTitle>
             </div>
           </CardHeader>
@@ -271,4 +343,12 @@ type ClusterWiseCardProps = {
 type AppWiseCardProps = {
   protectedPVCData: ProtectedPVCData[];
   selectedApplication: ProtectedAppsMap;
+};
+
+type ClusterAppCardTitleProps = {
+  app: ApplicationObj;
+  cluster: string;
+  appKind: string;
+  appAPIVersion: string;
+  appType: APPLICATION_TYPE;
 };

packages/mco/components/modals/app-manage-policies/policy-list-view.tsx
@@ -3,6 +3,7 @@ import { ActionDropdown } from '@odf/shared/dropdown/action-dropdown';
 import { ModalBody } from '@odf/shared/modals/Modal';
 import { getName } from '@odf/shared/selectors';
 import { useCustomTranslation } from '@odf/shared/useCustomTranslationHook';
+import { getPageRange } from '@odf/shared/utils';
 import { getErrorMessage } from '@odf/shared/utils';
 import { global_palette_blue_300 as blueInfoColor } from '@patternfly/react-tokens/dist/js/global_palette_blue_300';
 import {
@@ -38,12 +39,6 @@ import './style.scss';
 
 const INITIAL_PAGE_NUMBER = 1;
 const COUNT_PER_PAGE_NUMBER = 4;
-
-const getRange = (currentPage: number, perPage: number) => {
-  const indexOfLastRow = currentPage * perPage;
-  const indexOfFirstRow = indexOfLastRow - perPage;
-  return [indexOfFirstRow, indexOfLastRow];
-};
 
 const filterPolicies = (dataPolicyInfo: DataPolicyType[], searchText: string) =>
   dataPolicyInfo.filter((policy) =>
@@ -130,7 +125,7 @@ export const PolicyListView: React.FC<PolicyListViewProps> = ({
   const [page, setPage] = React.useState(INITIAL_PAGE_NUMBER);
   const [perPage, setPerPage] = React.useState(COUNT_PER_PAGE_NUMBER);
   const [searchText, onSearchChange] = React.useState('');
-  const [start, end] = getRange(page, perPage);
+  const [start, end] = getPageRange(page, perPage);
   const policies = filterPolicies(dataPolicyInfo, searchText) || [];
   const paginatedPolicies = policies.slice(start, end);
   const unFilteredAssignedPolicyLength = dataPolicyInfo.length;