7dcda89c24244312192c054908800c7010f37a13
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2024-01-08 14:52:56 +0530

    [RDR] Optimise cluster for RDR dialog fails with error msg
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- packages/ocs/dashboards/persistent-internal/status-card/osd-migration/osd-migration-progress.tsx
+++ packages/ocs/dashboards/persistent-internal/status-card/osd-migration/osd-migration-progress.tsx
@@ -10,10 +10,7 @@ import { CephClusterKind } from '@odf/shared/types';
 import { useCustomTranslation } from '@odf/shared/useCustomTranslationHook';
 import { ViewDocumentation } from '@odf/shared/utils';
 import { HealthState } from '@openshift-console/dynamic-plugin-sdk';
-import {
-  HealthBody,
-  HealthItem,
-} from '@openshift-console/dynamic-plugin-sdk-internal';
+import { HealthItem } from '@openshift-console/dynamic-plugin-sdk-internal';
 import * as _ from 'lodash-es';
 import { Divider, Flex, FlexItem } from '@patternfly/react-core';
 import { InProgressIcon } from '@patternfly/react-icons';
@@ -60,78 +57,76 @@ export const OSDMigrationProgress: React.FC<OSDMigrationProgressProps> = ({
   return (
     <>
       {migrationStatus !== OSDMigrationStatus.PENDING && <Divider />}
-      <HealthBody>
-        <Flex alignItems={{ default: 'alignItemsCenter' }}>
-          {migrationStatus === OSDMigrationStatus.COMPLETED && (
-            <>
-              <FlexItem>
-                <HealthItem
-                  title={t('Cluster ready for Regional-DR setup.')}
-                  state={HealthState.OK}
-                />
-              </FlexItem>
-              <FlexItem>
-                <ViewDocumentation
-                  text={t('Setting up disaster recovery')}
-                  doclink={ODF_DR_DOC_HOME}
-                />
-              </FlexItem>
-            </>
-          )}
+      <Flex alignItems={{ default: 'alignItemsCenter' }}>
+        {migrationStatus === OSDMigrationStatus.COMPLETED && (
+          <>
+            <FlexItem>
+              <HealthItem
+                title={t('Cluster ready for Regional-DR setup.')}
+                state={HealthState.OK}
+              />
+            </FlexItem>
+            <FlexItem>
+              <ViewDocumentation
+                text={t('Setting up disaster recovery')}
+                doclink={ODF_DR_DOC_HOME}
+              />
+            </FlexItem>
+          </>
+        )}
 
-          {migrationStatus === OSDMigrationStatus.IN_PROGRESS && (
-            <>
-              <FlexItem className="pf-u-mt-xl">
-                <HealthItem
-                  icon={<InProgressIcon className="co-dashboard-icon" />}
-                  state={HealthState.OK}
-                  title={t('Cluster OSDs are being migrated')}
-                />
-              </FlexItem>
-              <FlexItem className="pf-u-mt-xl">
-                {t(
-                  '{{ percentageComplete }}% completed ({{ migratedDevices }}/{{ totalOsd }} remaining)',
-                  {
-                    percentageComplete,
-                    migratedDevices,
-                    totalOsd,
-                  }
-                )}
-              </FlexItem>
-            </>
-          )}
+        {migrationStatus === OSDMigrationStatus.IN_PROGRESS && (
+          <>
+            <FlexItem className="pf-u-mt-xl">
+              <HealthItem
+                icon={<InProgressIcon className="co-dashboard-icon" />}
+                state={HealthState.OK}
+                title={t('Cluster OSDs are being migrated')}
+              />
+            </FlexItem>
+            <FlexItem className="pf-u-mt-xl">
+              {t(
+                '{{ percentageComplete }}% completed ({{ migratedDevices }}/{{ totalOsd }} remaining)',
+                {
+                  percentageComplete,
+                  migratedDevices,
+                  totalOsd,
+                }
+              )}
+            </FlexItem>
+          </>
+        )}
 
-          {migrationStatus === OSDMigrationStatus.FAILED && (
-            <>
-              <FlexItem>
-                <HealthItem
-                  state={HealthState.OK}
-                  icon={
-                    <RedExclamationCircleIcon className="co-dashboard-icon" />
-                  }
-                  title={t('Could not migrate cluster OSDs.')}
-                />
-              </FlexItem>
-              <FlexItem>
-                <ViewDocumentation
-                  text={t('Check documentation')}
-                  doclink={ODF_DR_DOC_HOME}
-                />
-              </FlexItem>
-              <FlexItem align={{ default: 'alignRight' }}>
-                {t(
-                  '{{ percentageComplete }}% completed ({{ migratedDevices }}/{{ totalOsd }} remaining)',
-                  {
-                    percentageComplete,
-                    migratedDevices,
-                    totalOsd,
-                  }
-                )}
-              </FlexItem>
-            </>
-          )}
-        </Flex>
-      </HealthBody>
+        {migrationStatus === OSDMigrationStatus.FAILED && (
+          <>
+            <FlexItem>
+              <HealthItem
+                state={HealthState.OK}
+                icon={
+                  <RedExclamationCircleIcon className="co-dashboard-icon" />
+                }
+                title={t('Could not migrate cluster OSDs.')}
+              />
+            </FlexItem>
+            <FlexItem>
+              <ViewDocumentation
+                text={t('Check documentation')}
+                doclink={ODF_DR_DOC_HOME}
+              />
+            </FlexItem>
+            <FlexItem align={{ default: 'alignRight' }}>
+              {t(
+                '{{ percentageComplete }}% completed ({{ migratedDevices }}/{{ totalOsd }} remaining)',
+                {
+                  percentageComplete,
+                  migratedDevices,
+                  totalOsd,
+                }
+              )}
+            </FlexItem>
+          </>
+        )}
+      </Flex>
     </>
   );
 };
