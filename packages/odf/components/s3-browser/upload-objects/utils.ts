a09b1fe226d3a73d93167c4c49698b306c5810e6
Bipul Adhikari <badhikar@redhat.com>
2024-11-26 20:58:00 +0545

    Moves upload component to mobX
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- packages/odf/components/s3-browser/upload-objects/utils.ts
+++ packages/odf/components/s3-browser/upload-objects/utils.ts
@@ -1,32 +1,27 @@
-import {
-  humanizeBinaryBytes,
-  humanizeDecimalBytesPerSec,
-  humanizeMinutes,
-  humanizeSeconds,
-} from '@odf/shared/utils';
+import { humanizeMinutes, humanizeSeconds } from '@odf/shared/utils';
 import * as _ from 'lodash-es';
-import { UploadProgress, UploadStatus } from './types';
+import { UploadProgress, UploadProgressBatch, UploadStatus } from './types';
 
-export const getCompletedAndTotalUploadCount = (objects: UploadProgress) => {
+export const getCompletedAndTotalUploadCount = (
+  objects: UploadProgressBatch
+) => {
   const totalObjects = Object.keys(objects).length;
-  const totalUploaded = Object.values(objects).filter(
-    (obj) => obj.uploadState === UploadStatus.UPLOAD_COMPLETE
-  ).length;
+  const totalUploaded = Object.values(objects).filter(isCompletedFile).length;
   return [totalUploaded, totalObjects];
 };
 
 export const getCompletedTotalFailedCount = (
-  uploadProgress: UploadProgress
+  uploadProgress: UploadProgressBatch
 ) => {
   const progressItems = Object.values(uploadProgress);
   const [completedUploads, failedUploads] = progressItems.reduce(
     (acc, curr) => {
-      const isCompleted = curr.uploadState === UploadStatus.UPLOAD_COMPLETE;
+      const isCompleted = isCompletedFile(curr);
       if (isCompleted) {
         acc = [acc[0] + 1, acc[1]];
         return acc;
       }
-      const isFailed = curr.uploadState === UploadStatus.UPLOAD_FAILED;
+      const isFailed = isFailedFile(curr) || isCancelledFile(curr);
       if (isFailed) {
         acc = [acc[0], acc[1] + 1];
         return acc;
@@ -38,68 +33,8 @@ export const getCompletedTotalFailedCount = (
   return [completedUploads, progressItems.length, failedUploads];
 };
 
-export const getFailedFiles = (objects: UploadProgress) =>
-  Object.values(objects).filter(
-    (obj) => obj.uploadState === UploadStatus.UPLOAD_FAILED
-  ).length;
-
-export const getUploadSpeed = (objects: UploadProgress): string => {
-  if (_.isEmpty(objects)) return '';
-  const files = Object.values(objects);
-  const uploadingFiles = files.filter(
-    (item) =>
-      item.uploadState === UploadStatus.UPLOAD_START ||
-      item.uploadState === UploadStatus.INIT_STATE
-  );
-  const totalTimes = uploadingFiles.map((file) => Date.now() - file?.startTime);
-  const totalUploaded = uploadingFiles.map((file) => file.loaded ?? 0);
-  const uploadSpeeds = totalUploaded.map((size, i) => size / totalTimes[i]);
-  const speed = _.sum(uploadSpeeds) / uploadSpeeds.length;
-  return humanizeDecimalBytesPerSec(speed * 1000).string;
-};
-
-export const getTotalRemainingFilesAndSize = (
-  objects: UploadProgress
-): string => {
-  if (_.isEmpty(objects)) return '';
-  const files = Object.values(objects);
-  const uploadingFiles = files.filter(
-    (item) =>
-      item.uploadState === UploadStatus.UPLOAD_START ||
-      item.uploadState === UploadStatus.INIT_STATE
-  );
-  const filesCount = uploadingFiles.length;
-  const filesSize = uploadingFiles.reduce(
-    (acc, curr) => (acc += curr.total - curr.loaded),
-    0
-  );
-  return `${filesCount} files (${humanizeBinaryBytes(filesSize).string})`;
-};
-
-export const getTotalTimeRemaining = (objects: UploadProgress): string => {
-  const files = Object.values(objects);
-  const uploadingFiles = files.filter(
-    (item) =>
-      item.uploadState === UploadStatus.UPLOAD_START ||
-      item.uploadState === UploadStatus.INIT_STATE
-  );
-  const totalTimes = uploadingFiles.map((file) => Date.now() - file?.startTime);
-  const totalUploaded = uploadingFiles.map((file) => file.loaded ?? 0);
-  const uploadSpeeds = totalUploaded.map((size, i) => size / totalTimes[i]);
-  const speed = _.sum(uploadSpeeds) / uploadSpeeds.length;
-  const totalRemainingBytes = uploadingFiles.reduce(
-    (acc, curr) => (acc += curr.total - curr.loaded),
-    0
-  );
-  const timeRemaining = totalRemainingBytes / speed;
-  if (timeRemaining < 1000) {
-    return humanizeSeconds(timeRemaining, 'ms').string;
-  }
-  return humanizeMinutes(timeRemaining / 1000).string;
-};
-
 export const getTotalTimeElapsed = (
-  objects: UploadProgress,
+  objects: UploadProgressBatch,
   completionTime: number
 ): string => {
   const earliestTime = Object.values(objects).reduce((acc, curr) => {
@@ -107,9 +42,23 @@ export const getTotalTimeElapsed = (
       return curr.startTime;
     } else return acc;
   }, Number.MAX_SAFE_INTEGER);
-  const fromNow = completionTime - earliestTime;
+  let fromNow = completionTime - earliestTime;
+  fromNow = fromNow < 0 ? 0 : fromNow;
   const minutes = humanizeMinutes(fromNow / 1000);
   if (minutes.value > 1) {
     return minutes.string;
   } else return humanizeSeconds(fromNow / 1000, 'seconds').string;
 };
+
+export const isUploadingFile = (upload: UploadProgress): boolean =>
+  upload.uploadState === UploadStatus.INIT_STATE ||
+  upload.uploadState === UploadStatus.UPLOAD_START;
+
+export const isFailedFile = (upload: UploadProgress): boolean =>
+  upload.uploadState === UploadStatus.UPLOAD_FAILED;
+
+export const isCancelledFile = (upload: UploadProgress): boolean =>
+  upload.uploadState === UploadStatus.UPLOAD_CANCELLED;
+
+export const isCompletedFile = (upload: UploadProgress): boolean =>
+  upload.uploadState === UploadStatus.UPLOAD_COMPLETE;
--- packages/odf/modals/s3-browser/abort-uploads/AbortUploadsModal.tsx
+++ packages/odf/modals/s3-browser/abort-uploads/AbortUploadsModal.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ButtonBar, useCustomTranslation } from '@odf/shared';
+import { observer } from 'mobx-react-lite';
 import { Trans } from 'react-i18next';
 import {
   Button,
@@ -7,54 +8,60 @@ import {
   Modal,
   ModalVariant,
 } from '@patternfly/react-core';
+import { uploadStore } from '../../../components/s3-browser/upload-objects/store';
 
 type AbortUploadsModalProps = {
-  abortAll: () => Promise<void>;
+  abortAll: () => void;
 };
 
-export const AbortUploadsModal: React.FC<AbortUploadsModalProps> = ({
-  abortAll,
-}) => {
-  const { t } = useCustomTranslation();
-  const [isModalOpen, setModalOpen] = React.useState(false);
+export const AbortUploadsModal: React.FC<AbortUploadsModalProps> = observer(
+  ({ abortAll }) => {
+    const { t } = useCustomTranslation();
+    const [isModalOpen, setModalOpen] = React.useState(false);
+    const onAbort = () => {
+      abortAll();
+      uploadStore.abortAll();
+      setModalOpen(false);
+    };
 
-  return (
-    <>
-      <Button variant={ButtonVariant.link} onClick={() => setModalOpen(true)}>
-        {t('Cancel upload')}
-      </Button>
-      <Modal
-        title={t('Cancel all ongoing uploads?')}
-        titleIconVariant="warning"
-        isOpen={isModalOpen}
-        variant={ModalVariant.medium}
-        actions={[
-          <ButtonBar inProgress={false} errorMessage={null}>
-            <span>
-              <Button
-                variant={ButtonVariant.primary}
-                onClick={abortAll}
-                className="pf-v5-u-mr-xs"
-              >
-                {t('Yes, cancel')}
-              </Button>
-              <Button
-                variant={ButtonVariant.secondary}
-                onClick={() => setModalOpen(false)}
-                className="pf-v5-u-ml-xs"
-              >
-                {t('No, continue uploads')}
-              </Button>
-            </span>
-          </ButtonBar>,
-        ]}
-      >
-        <Trans t={t}>
-          Are you sure you want to cancel the ongoing uploads? Any files
-          currently being uploaded will be stopped, and partially uploaded files
-          will not be saved.
-        </Trans>
-      </Modal>
-    </>
-  );
-};
+    return (
+      <>
+        <Button variant={ButtonVariant.link} onClick={() => setModalOpen(true)}>
+          {t('Cancel upload')}
+        </Button>
+        <Modal
+          title={t('Cancel all ongoing uploads?')}
+          titleIconVariant="warning"
+          isOpen={isModalOpen}
+          variant={ModalVariant.medium}
+          actions={[
+            <ButtonBar inProgress={false} errorMessage={null}>
+              <span>
+                <Button
+                  variant={ButtonVariant.primary}
+                  onClick={onAbort}
+                  className="pf-v5-u-mr-xs"
+                >
+                  {t('Yes, cancel')}
+                </Button>
+                <Button
+                  variant={ButtonVariant.secondary}
+                  onClick={() => setModalOpen(false)}
+                  className="pf-v5-u-ml-xs"
+                >
+                  {t('No, continue uploads')}
+                </Button>
+              </span>
+            </ButtonBar>,
+          ]}
+        >
+          <Trans t={t}>
+            Are you sure you want to cancel the ongoing uploads? Any files
+            currently being uploaded will be stopped, and partially uploaded
+            files will not be saved.
+          </Trans>
+        </Modal>
+      </>
+    );
+  }
+);
