3c6b54381d0e322e24eb788d11e7e6477323378a
Alfonso Martínez <alfonsomthd@protonmail.com>
2025-01-23 11:15:10 +0100

    Object Browser: access S3 endpoint via console proxy

--- packages/mco/constants/url-paths.ts
+++ packages/mco/constants/url-paths.ts
@@ -1,13 +1,16 @@
+import {
+  CONSOLE_PROXY_ROOT_PATH,
+  MCO_PROXY_ROOT_PATH,
+} from '@odf/shared/constants/common';
+
 // ACM thanos proxy endpoint
-export const ACM_ENDPOINT =
-  '/api/proxy/plugin/odf-multicluster-console/acm-thanos-querier';
+export const ACM_ENDPOINT = `${MCO_PROXY_ROOT_PATH}/acm-thanos-querier`;
 // ACM thanos dev endpoint
 export const DEV_ACM_ENDPOINT = '/acm-thanos-querier';
 // ACM application details page endpoint
 export const applicationDetails =
   '/multicloud/applications/details/:namespace/:name';
 // ACM search api proxy endpoint
-export const ACM_SEARCH_PROXY_ENDPOINT =
-  '/api/proxy/plugin/acm/console/multicloud/proxy/search';
+export const ACM_SEARCH_PROXY_ENDPOINT = `${CONSOLE_PROXY_ROOT_PATH}/acm/console/multicloud/proxy/search`;
 // MCO DR navigation item base route
 export const DR_BASE_ROUTE = '/multicloud/data-services/disaster-recovery';
