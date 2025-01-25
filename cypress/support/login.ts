95800e0bfa84c34e62aa8ea8f7f66d0ba079d2e9
SanjalKatiyar <sanjaldhir@gmail.com>
2025-01-25 22:20:56 +0530

    1234

--- cypress/support/login.ts
+++ cypress/support/login.ts
@@ -22,7 +22,6 @@ Cypress.Commands.add(
       () => {
         // Check if auth is disabled (for a local development environment).
         cy.visit(''); // visits baseUrl which is set in plugins.js
-
         cy.window().then((win: any) => {
           if (win.SERVER_FLAGS?.authDisabled) {
             cy.task(
@@ -35,30 +34,56 @@ Cypress.Commands.add(
           cy.log(`window object location ${win?.location}`);
           cy.log(`OAUTH_BASE_ADDRESS ${Cypress.env('OAUTH_BASE_ADDRESS')}`);
 
-          cy.origin(Cypress.env('OAUTH_BASE_ADDRESS'), () => {
-            const idp = provider || KUBEADMIN_IDP;
+          cy.origin(
+            Cypress.env('OAUTH_BASE_ADDRESS'),
+            {
+              args: {
+                provider,
+                username,
+                password,
+                KUBEADMIN_IDP,
+                KUBEADMIN_USERNAME,
+                submitButton,
+              },
+            },
+            ({
+              /* eslint-disable @typescript-eslint/no-shadow, @typescript-eslint/naming-convention */
+              provider,
+              username,
+              password,
+              KUBEADMIN_IDP,
+              KUBEADMIN_USERNAME,
+              submitButton,
+              /* eslint-enable @typescript-eslint/no-shadow, @typescript-eslint/naming-convention */
+            }) => {
+              const idp = provider || KUBEADMIN_IDP;
 
-            cy.task('log', `  Logging in as ${username || KUBEADMIN_USERNAME}`);
+              cy.task(
+                'log',
+                `  Logging in as ${username || KUBEADMIN_USERNAME}`
+              );
+              cy.byLegacyTestID('login', { timeout: 10000 }).should(
+                'be.visible'
+              );
 
-            cy.byLegacyTestID('login', { timeout: 10000 }).should('be.visible');
+              // eslint-disable-next-line cypress/require-data-selectors
+              cy.get('body').then(($body) => {
+                if ($body.text().includes(idp)) {
+                  cy.contains(idp).should('be.visible').click();
+                }
+              });
 
-            // eslint-disable-next-line cypress/require-data-selectors
-            cy.get('body').then(($body) => {
-              if ($body.text().includes(idp)) {
-                cy.contains(idp).should('be.visible').click();
-              }
-            });
-
-            /* eslint-disable cypress/require-data-selectors */
-            cy.get('#inputUsername').type(username || KUBEADMIN_USERNAME);
-            cy.get('#inputPassword').type(
-              password || Cypress.env('BRIDGE_KUBEADMIN_PASSWORD'),
-              {
-                log: false,
-              }
-            );
-            cy.get(submitButton).click();
-          });
+              /* eslint-disable cypress/require-data-selectors */
+              cy.get('#inputUsername').type(username || KUBEADMIN_USERNAME);
+              cy.get('#inputPassword').type(
+                password || Cypress.env('BRIDGE_KUBEADMIN_PASSWORD'),
+                {
+                  log: false,
+                }
+              );
+              cy.get(submitButton).click();
+            }
+          );
 
           // Back to the console's origin and ensure user is logged in.
           cy.visit('');
