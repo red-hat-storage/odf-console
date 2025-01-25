5bfbe825c26dc9a391e60c266e609f0e6ad86302
SanjalKatiyar <sanjaldhir@gmail.com>
2025-01-25 20:24:12 +0530

    123

--- cypress/support/login.ts
+++ cypress/support/login.ts
@@ -32,35 +32,36 @@ Cypress.Commands.add(
             return;
           }
 
-          const idp = provider || KUBEADMIN_IDP;
-          cy.task('log', `  Logging in as ${username || KUBEADMIN_USERNAME}`);
-
-          // eslint-disable-next-line cypress/no-unnecessary-waiting
-          cy.wait(5000);
-          cy.task('log', `window object location ${win?.location}`);
           cy.log(`window object location ${win?.location}`);
-          cy.task(
-            'log',
-            `OAUTH_BASE_ADDRESS ${Cypress.env('OAUTH_BASE_ADDRESS')}`
-          );
           cy.log(`OAUTH_BASE_ADDRESS ${Cypress.env('OAUTH_BASE_ADDRESS')}`);
 
-          cy.byLegacyTestID('login', { timeout: 10000 }).should('be.visible');
-          // eslint-disable-next-line cypress/require-data-selectors
-          cy.get('body').then(($body) => {
-            if ($body.text().includes(idp)) {
-              cy.contains(idp).should('be.visible').click();
-            }
+          cy.origin(Cypress.env('OAUTH_BASE_ADDRESS'), () => {
+            const idp = provider || KUBEADMIN_IDP;
+
+            cy.task('log', `  Logging in as ${username || KUBEADMIN_USERNAME}`);
+
+            cy.byLegacyTestID('login', { timeout: 10000 }).should('be.visible');
+
+            // eslint-disable-next-line cypress/require-data-selectors
+            cy.get('body').then(($body) => {
+              if ($body.text().includes(idp)) {
+                cy.contains(idp).should('be.visible').click();
+              }
+            });
+
+            /* eslint-disable cypress/require-data-selectors */
+            cy.get('#inputUsername').type(username || KUBEADMIN_USERNAME);
+            cy.get('#inputPassword').type(
+              password || Cypress.env('BRIDGE_KUBEADMIN_PASSWORD'),
+              {
+                log: false,
+              }
+            );
+            cy.get(submitButton).click();
           });
-          /* eslint-disable cypress/require-data-selectors */
-          cy.get('#inputUsername').type(username || KUBEADMIN_USERNAME);
-          cy.get('#inputPassword').type(
-            password || Cypress.env('BRIDGE_KUBEADMIN_PASSWORD'),
-            {
-              log: false,
-            }
-          );
-          cy.get(submitButton).click();
+
+          // Back to the console's origin and ensure user is logged in.
+          cy.visit('');
           /* eslint-enable cypress/require-data-selectors */
           masthead.username.shouldBeVisible();
         });
