e96b17784b52692ce20f944a58b666b7858eea4b
Arun Kumar Mohan <amohan@redhat.com>
2025-03-15 10:11:49 +0530

    Adding CORS rules list page
    Signed-off-by: Arun Kumar Mohan <amohan@redhat.com>

--- packages/shared/src/s3/utils.ts
+++ packages/shared/src/s3/utils.ts
@@ -1,2 +1,5 @@
 export const isNoLifecycleRuleError = (error) =>
   error?.name === 'NoSuchLifecycleConfiguration';
+
+// TODO: verify whether 'name' is equal to'NoSuchCors'
+export const isNoCorsRuleError = (error) => error?.name === 'NoSuchCORS';
