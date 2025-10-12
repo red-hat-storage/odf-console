525269f6d8415667f8630075dd081ab6e625ce38
Harshvardhan Handa <harshvardhanhanda@Harshvardhans-MacBook-Pro.local>
2025-10-12 16:48:24 +0530

    Adding multus.tsx

--- packages/odf/components/create-storage-system/create-storage-system-steps/security-and-network-step/multus.tsx
+++ packages/odf/components/create-storage-system/create-storage-system-steps/security-and-network-step/multus.tsx
@@ -20,7 +20,6 @@ import {
   WatchK8sResults,
 } from '@openshift-console/dynamic-plugin-sdk';
 import * as _ from 'lodash-es';
-import { SelectOption } from '@patternfly/react-core';
 import {
   Alert,
   AlertVariant,
