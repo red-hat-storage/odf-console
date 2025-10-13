64dcd9221563c32d16eecd8ad0c28725ba96f9e5
vbadrina <vbadrina@redhat.com>
2025-10-13 17:54:25 +0530

    Add support for IBM scale provisioner
    Signed-off-by: vbadrina <vbadrina@redhat.com>

--- packages/mco/hooks/use-storage-provisioner.ts
+++ packages/mco/hooks/use-storage-provisioner.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CEPH_PROVISIONERS } from '@odf/shared';
+import { CEPH_PROVISIONERS, IBM_PROVISIONERS } from '@odf/shared';
 import { queryStorageClassesUsingClusterNames } from '../utils';
 import { useACMSafeFetch } from './acm-safe-fetch';
 
@@ -53,14 +53,16 @@ export function useStorageProvisioners(clusters: string[]): {
   const providersByCluster = React.useMemo<ClusterProviders[]>(() => {
     if (!loaded) return [];
 
+    const supportedProvisioners = [...CEPH_PROVISIONERS, ...IBM_PROVISIONERS];
+
     return Object.entries(itemsByCluster).map(([cluster, items]) => {
       const odfCount = items.filter((i) =>
-        CEPH_PROVISIONERS.includes(i.provisioner)
+        supportedProvisioners.includes(i.provisioner)
       ).length;
 
       const tpMap = new Map<string, number>();
       items.forEach(({ provisioner }) => {
-        if (!CEPH_PROVISIONERS.includes(provisioner)) {
+        if (!supportedProvisioners.includes(provisioner)) {
           tpMap.set(provisioner, (tpMap.get(provisioner) || 0) + 1);
         }
       });
