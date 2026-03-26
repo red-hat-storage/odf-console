20c9c33463f7a7cf8794cb12d87c7517ab837c67
Bipul Adhikari <badhikar@redhat.com>
2026-03-26 13:50:02 +0545

    Adds resource limits for SAN systems
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- packages/odf/components/create-storage-system/external-systems/common/payload.ts
+++ packages/odf/components/create-storage-system/external-systems/common/payload.ts
@@ -53,7 +53,8 @@ export const labelNodes = (nodes: WizardNodeState[]) => {
 
 export const createScaleLocalClusterPayload = (
   externalKmmRegistry?: ExternalKMMRegistryConfig,
-  addClusterLabels?: boolean
+  addClusterLabels?: boolean,
+  addResourceLimits?: boolean
 ) => {
   const spec: ClusterKind['spec'] = {
     daemon: {
@@ -108,6 +109,27 @@ export const createScaleLocalClusterPayload = (
     };
   }
 
+  if (addResourceLimits) {
+    spec.daemon = {
+      roles: [
+        {
+          limits: {},
+          name: 'client',
+          profile: {},
+          resources: {
+            cpu: '4',
+            memory: '40540114124',
+          },
+        },
+        {
+          limits: {},
+          name: 'storage',
+          profile: {},
+          resources: {},
+        },
+      ],
+    };
+  }
   const payload: ClusterKind = {
     apiVersion: 'scale.spectrum.ibm.com/v1beta1',
     kind: 'Cluster',
