bd9f5168afd1beb866813186d1b476ca36535c27
Bipul Adhikari <badhikar@redhat.com>
2026-05-08 08:43:56 +0545

    Adds support for multiple Scale remote clusters
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- packages/odf/components/ibm-common/utils.ts
+++ packages/odf/components/ibm-common/utils.ts
@@ -2,10 +2,14 @@ import { FileSystemKind } from '@odf/core/types/scale';
 import * as _ from 'lodash-es';
 
 export const filterScaleFileSystems = (
-  fileSystems: FileSystemKind[]
+  fileSystems: FileSystemKind[],
+  remoteFileSystemName: string
 ): FileSystemKind[] => {
   return fileSystems.filter(
-    (fs) => fs.spec.remote && !_.isEmpty(fs.spec.remote)
+    (fs) =>
+      fs.spec.remote &&
+      !_.isEmpty(fs.spec.remote) &&
+      fs.spec.remote.cluster === remoteFileSystemName
   );
 };
 
